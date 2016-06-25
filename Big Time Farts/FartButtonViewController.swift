3084e1813d518ca59458bb5d5be2928787fdb561
Perris Davis <perrisdavis@me.com>
2016-06-24 20:13:08 -0700

    Added audio player and menu modal.

--- Big Time Farts/FartButtonViewController.swift
+++ Big Time Farts/FartButtonViewController.swift
@@ -26,20 +26,53 @@ class FartButtonViewController: UIViewController {
         return button
     }()
     
+    let menuButton: UIButton = {
+        
+        let button = UIButton()
+        button.backgroundColor = UIColor.greenColor()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var fartPlayer: AVAudioPlayer = {
+        let player = AVAudioPlayer()
+        return player
+    
+    }()
+    
+    let fartSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("test-fart", ofType: "mp3")!)
+    
     func setupButton() {
     
         self.view.addSubview(fartButton)
-    
+        self.view.addSubview(menuButton)
         fartButton.addTarget(self, action: #selector(FartButtonViewController.playFart), forControlEvents: .TouchUpInside)
+        menuButton.addTarget(self, action: #selector(FartButtonViewController.showMenu), forControlEvents: .TouchUpInside)
 
-        let viewsDictionary = ["fartButton": fartButton]
-        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[fartButton]-20-|", options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
-        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-20-[fartButton]-20-|", options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+        let viewsDictionary = ["v0": fartButton, "v1": menuButton]
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[v0]-20-|", options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[v1]-20-|", options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-20-[v0]-20-[v1]-20-|", options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
     }
     
     func playFart() {
+        
+        do {
+            
+            self.fartPlayer = try AVAudioPlayer(contentsOfURL: fartSound)
+            self.fartPlayer.prepareToPlay()
+            self.fartPlayer.play()
+        } catch {
+        
+            print("Error getting the fart file")
+        }
+    }
     
-        print("poop")
+    func showMenu() {
+    
+        let menuViewController = MenuViewController()
+        menuViewController.modalPresentationStyle = .OverCurrentContext
+        self.presentViewController(menuViewController, animated: true, completion: nil)
     }
     
     
---
+++ Big Time Farts/MenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuViewController.swift
+//  Big Time Farts
+//
+//  Created by Perris Davis on 6/24/16.
+//  Copyright © 2016 Wikipedia Brown. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.purpleColor()
+    }
+
+}
