d3860d1fc8d365bd9d6dcdc5a841c1a4c81e697b
Perris Davis <perrisdavis@me.com>
2016-07-04 22:21:08 -0700

    Commented out data model.

--- Big Time Farts/Fart.swift
+++ Big Time Farts/Fart.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2016 Wikipedia Brown. All rights reserved.
 //
 
-import Foundation
-import CoreData
-
-
-class Fart: NSManagedObject {
-
-// Insert code here to add functionality to your managed object subclass
-
-}
+//import Foundation
+//import CoreData
+//
+//
+//class Fart: NSManagedObject {
+//
+//// Insert code here to add functionality to your managed object subclass
+//
+//}
